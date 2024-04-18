0188f66444c2a0e35f6fb5fb19c56fb6a7b747be
Martin <marindidolich2012@gmail.com>
2024-04-18 14:26:22 -0300

    Arreglado validacion de matrices

--- Ejercicio2/Ejercicio2.sh
+++ Ejercicio2/Ejercicio2.sh
@@ -2,10 +2,10 @@
 
 
 ayuda() {
-    echo "\n-m1, --matriz1, ruta del archivo con la matriz1, debe ser valido\n"
-    echo "-m2, --matriz2, ruta del archivo con la matriz2, debe ser valido\n"
-    echo "-s, Caracter separador de valores, por defecto es ';', no puede ser '-'\n"
-    echo "-h, --help ayuda\n"
+    echo -e "\n-m1, --matriz1, ruta del archivo con la matriz1, debe ser valido\n"
+    echo -e "-m2, --matriz2, ruta del archivo con la matriz2, debe ser valido\n"
+    echo -e "-s, Caracter separador de valores, por defecto es ',', no puede ser '-', el separador debe pasarse entr "" \n"
+    echo -e "-h, --help ayuda\n"
 }
 
 options=$(getopt -o m,1:,2:,s:,h, --l help,matriz1:,matriz2:,separador: -- "$@" 2> /dev/null)
@@ -53,7 +53,7 @@ if [ $rm1 = "false" ] || [ $rm2 = "false" ]; then
 fi
 
 if [ $s = "false" ]; then
- s=";"
+ s=","
 fi
 
 if [ $s = '-' ]; then
@@ -68,7 +68,7 @@ fi
 
 #Valido que las matrices sean validas con awk
 
-validacion1=$(awk -v RS="\r" -F"$s" '
+validacion1=$(awk -v RS="\r\n|\n" -F"$s" '
 
     BEGIN {
         esValida=1
@@ -76,7 +76,6 @@ validacion1=$(awk -v RS="\r" -F"$s" '
     }
     
     {
-        print $0
         if (NF == 0) {
             esValida=0
         }
@@ -90,6 +89,7 @@ validacion1=$(awk -v RS="\r" -F"$s" '
                 esValida=0
             }
         }
+        cantCamposAnt = NF
     }
     
     END {
@@ -97,41 +97,44 @@ validacion1=$(awk -v RS="\r" -F"$s" '
     }
 ' "$rm1")
 
-# validacion2=$(awk -v RS="\r" -F"$s" '
+validacion2=$(awk -v RS="\r\n|\n" -F"$s" '
 
-#     BEGIN {
-#         esValida=1
-#         cantCamposAnt=0
-#     }
+    BEGIN {
+        esValida=1
+        cantCamposAnt=0
+    }
     
-#     {
-#         if (NF == 0) {
-#             esValida=0
-#         }
+    {
+        if (NF == 0) {
+            esValida=0
+        }
 
-#         if (cantCamposAnt != 0 && cantCamposAnt != NF) {
-#             esValida=0
-#         }
+        if (cantCamposAnt != 0 && cantCamposAnt != NF) {
+            esValida=0
+        }
 
-#         for (i=1; i<=NF; i++) {
-#             if ($i !~ /^[[:space:]]*[-]?[0-9]+[[:space:]]*$/) {
-#                 print $i
-#                 esValida=0
-#             }
-#         }
-
-#     }
+        for (i=1; i<=NF; i++) {
+            if ($i !~ /^[[:space:]]*[-]?[0-9]+[[:space:]]*$/) {
+                esValida=0
+            }
+        }
+        cantCamposAnt = NF
+    }
     
-#     END {
-#         if (esValida == 0) {
-#             print 1
-#         }else{
-#             print 0
-#         }
-#     }
-# ' "$rm2")
+    END {
+        print esValida
+    }
+' "$rm2")
 
-echo $validacion1
+if [ $validacion1 = 0 ]; then
+    echo "La matriz 1 no es valida"
+    exit 1
+fi
+
+if [ $validacion2 = 0 ]; then
+    echo "La matriz 2 no es valida"
+    exit 1
+fi
 
 declare -a matriz1
 
