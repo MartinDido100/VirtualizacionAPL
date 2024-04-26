098f0669ab65c2a811a15dc8f62613c1fadd8cef
Marcos Quelali <marcosz.abruzzi@gmail.com>
2024-04-25 22:49:44 -0300

    Ejercicio 3 ps final

--- Ejercicio 3/Ejercicio3 PS/Ejercicio3.ps1
+++ Ejercicio 3/Ejercicio3 PS/Ejercicio3.ps1
@@ -1,8 +1,44 @@
+<#
+.SYNOPSIS
+Analiza archivos de texto para contar palabras y caracteres, excluyendo los caracteres que especifique el usuario.
+
+.DESCRIPTION
+Cada archivo de texto en un directorio dado es analizado, contando la cantidad de palabras y caracteres, excluyendo aquellos caracteres especificados. También proporciona estadísticas como la palabra más repetida y la frecuencia de cada caracter.
+
+.PARAMETER directorio
+El directorio que contiene los archivos de texto a procesar. Este parámetro es obligatorio.
+
+.PARAMETER extension
+La extensión de los archivos de texto a procesar. Este parámetro es obligatorio.
+
+.PARAMETER omitir
+Una cadena de caracteres que se deben omitir durante el análisis. Si no se especifica, no se omitirá ningún caracter.
+
+.PARAMETER separador
+El separador utilizado para dividir el texto en palabras. El valor predeterminado es un espacio (' ').
+
+.EXAMPLE
+PS> Procesar-Archivos -directorio "C:\Textos" -extension "txt" -omitir "abc" -separador " "
+Este ejemplo procesa todos los archivos .txt en el directorio C:\Textos, omitiendo las letras a, b y c.
+
+.EXAMPLE
+PS> Procesar-Archivos -directorio "C:\Textos" -extension "txt"
+Este ejemplo procesa todos los archivos .txt en el directorio C:\Textos sin omitir ningún caracter.
+
+#>
+
 param(
     [Parameter(Mandatory=$true)]
-    [ValidateScript({Test-Path $_ -PathType 'Container'})]
+    [ValidateScript({
+        if (-not (Test-Path $_ -PathType 'Container')) {
+            throw "El directorio `'$($_)`' no existe. Por favor, verifica la ruta e inténtalo de nuevo."
+        }
+        return $true
+    })]
     [string]$directorio,
+
     [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
     [string]$extension,
     [string]$omitir,
     [string]$separador = ' '
@@ -23,6 +59,8 @@ function Procesar-Palabras {
         $palabrasPorCantCaracter = @{}
         $palabrasMayores = @{}
         $caracteres = @{}
+        $maxCaracteres = 0
+        $caracterMayor = ""
     }
 
     PROCESS {
@@ -37,7 +75,6 @@ function Procesar-Palabras {
                 }
                 $ocurrencias[$i]++
                 if ($null -eq $palabrasPorCantCaracter[$i.Length]) {
-                    Write-Host "Longitud de palabra: $i $($i.Length)"
                     $palabrasPorCantCaracter[$i.Length] = 0
                 }
                 $palabrasPorCantCaracter[$i.Length]++
@@ -46,6 +83,12 @@ function Procesar-Palabras {
                         $caracteres[$i[$nCaracter]] = 0
                     }
                     $caracteres[$i[$nCaracter]]++
+                    if($caracteres[$i[$nCaracter]] -gt $maxCaracteres) {
+                        $maxCaracteres = $caracteres[$i[$nCaracter]]
+                        $caracterMayor = $i[$nCaracter]
+                    } elseif ($caracteres[$i[$nCaracter]] -eq $maxCaracteres) {
+                        $caracterMayor += ", " + $i[$nCaracter]
+                    }
                 }
             }
         }
@@ -74,10 +117,18 @@ function Procesar-Palabras {
         Write-Host "Cantidad total de palabras: $cantTotal"
         Write-Host "Promedio de palabras por archivo: $($cantTotal / 3)"
 
-        foreach ($caracter in $caracteres.Keys) {
-            Write-Host "Caracter [$caracter] aparece $($caracteres[$caracter]) veces"
-        }
+        # foreach ($caracter in $caracteres.Keys) {
+        #     Write-Host "Caracter [$caracter] aparece $($caracteres[$caracter]) veces"
+        # }
+
+        Write-Host "Caracter/es mas repetido/s: $caracterMayor con $maxCaracteres ocurrencias"
     }
 }
 
-Get-Content "$directorio\*.$extension" -Encoding UTF8 | Procesar-Palabras -letrasOmitir "$omitir"
+$archivos = Get-ChildItem -Path "$directorio\*.$extension" -File
+
+if ($archivos.Count -gt 0) {
+    $archivos | Get-Content -Encoding UTF8 | Procesar-Palabras -letrasOmitir "$omitir"
+} else {
+    Write-Host "No se encontraron archivos con la extensión '$extension' en el directorio '$directorio'."
+}
